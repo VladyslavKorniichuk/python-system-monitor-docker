3299b8856f9a172dce6f821aaf280c555d762fa1
“Vladyslav <vlad.korniychuk12@gmail.com>
2026-02-19 17:47:40 +0100

    Fix flake8 issues

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import json
 import platform
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
 class MonitorHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        
+
         data = {
             "status": "success",
             "system": platform.system(),
@@ -15,8 +16,9 @@ class MonitorHandler(BaseHTTPRequestHandler):
             "machine": platform.machine(),
             "message": "Hello from Dockerized Python API!"
         }
-        
+
         self.wfile.write(json.dumps(data).encode('utf-8'))
+
 
 if __name__ == '__main__':
     server_address = ('', 8000)
